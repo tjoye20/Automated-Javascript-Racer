ef26d65dbbfc9fa4fdc49cb858b351358c9922b8
Tolu Oyeniyi <tjoye20@gmail.com>
2016-08-15 18:18:24 -0500

    Make game truly oo.

--- refactor.js
+++ refactor.js
@@ -1,6 +1,12 @@
-var Player = function(name) {
+var Player = function(name,domId) {
   this.name = name;
+  this.domId   = domId;
   this.position = 0;
+}
+
+Player.prototype.move = function(num){
+  this.domId.children().removeClass("active");
+  this.domId.children().eq(num).addClass("active");
 }
 
 var Game = function(player1, player2) {
@@ -9,9 +15,7 @@ var Game = function(player1, player2) {
 }
 
 Game.prototype.autoPlay = function() {
-  var id = setInterval(moveplayers, 1000);
   var num = Math.floor(Math.random() * 2) + 1;
-
   if (num == 2) {
     var random_num_1 = 2
     var random_num_2 = 1
@@ -20,40 +24,43 @@ Game.prototype.autoPlay = function() {
     var random_num_1 = 1
     var random_num_2 = 2
   };
-
-  function moveplayers() {
-
-    if (random_num_1 == 10) {
-      clearInterval(id);
-      var user_input = confirm("Game over!!! Player 1 wins! Do you want to play again?")
-      if (user_input == true) {
-        location.reload();
-      }
-    }
-    else if (random_num_2 == 10) {
-      clearInterval(id);
-      var user_input = confirm("Game over!!! Player 2 wins! Do you want to play again?")
-      if (user_input == true) {
-        location.reload();
-      }
-    }
-    else {
-      $("#player1_strip").children().removeClass("active");
-      $("#player1_strip").children().eq(random_num_1).addClass("active");
-
-      $("#player2_strip").children().removeClass("active");
-      $("#player2_strip").children().eq(random_num_2).addClass("active");
-
-      random_num_1++;
-      random_num_2++;
-    }
-  } //moveplayers
-
+  this.movePlayers(random_num_1,random_num_2)
 }
 
+Game.prototype.sayOver = function(player){
+  clearInterval(this.movePlayers);
+  var user_input = confirm("Game over!!! Player 1 wins! Do you want to play again?")
+  if (user_input == true) {
+    location.reload();
+  }
+}
+
+Game.prototype.isDone = function(random_num_1,random_num_2,interval){
+  if (random_num_1 == 10) {
+    this.sayOver(random_num_1);
+  }
+  else if (random_num_2 == 10) {
+    this.sayOver(random_num_2);
+  }
+}
+
+Game.prototype.movePlayers = function(random_num_1, random_num_2){
+  var player1 = this.player1;
+  var player2 = this.player2;
+  var game    = this;
+    setInterval(function(){
+      player1.move(random_num_1);
+      player2.move(random_num_2);
+      random_num_1++;
+      random_num_2++;
+      game.isDone(random_num_1,random_num_2,this)
+    }
+    , 1000)
+} //moveplayers
+
 $(document).ready(function() {
-  var player1 = new Player("Player 1");
-  var player2 = new Player("Player 2");
+  var player1 = new Player("Player 1",$("#player1_strip"));
+  var player2 = new Player("Player 2",$("#player2_strip"));
 
   var newGame = new Game(player1, player2);
 
